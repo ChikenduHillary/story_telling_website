57efa7fc7762fa641aafc7ddf5b4011c5bea32c8
ChikenduHillary <chikenduhillarychiagoziemu@gmail.com>
2025-01-16 17:02:08 +0100

    story telling website

---
+++ script.js
@@ -0,0 +1,29 @@
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all images and animate each one
+  document.querySelectorAll(".image").forEach((image) => {
+    gsap.from(image, {
+      y: 20,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: image, // Use the current image as trigger
+        toggleActions: "play pause resume reset",
+      },
+    });
+  });
+
+  // Select all headings and paragraphs and animate each one
+  document.querySelectorAll(".heading, .paragraph").forEach((element) => {
+    gsap.from(element, {
+      y: 10,
+      opacity: 0,
+      duration: 1,
+      scrollTrigger: {
+        trigger: element, // Use the current element as trigger
+        toggleActions: "play pause resume reset",
+      },
+    });
+  });
+});
